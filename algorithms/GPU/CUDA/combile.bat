bf1f103b7e37b7d2c951a22e5520be114fceb7e9
Tonto <tonto@winblows>
2025-10-22 20:45:46 +0300

    Fix cudart_static and add older computer archs

--- algorithms/GPU/CUDA/combile.bat
+++ algorithms/GPU/CUDA/combile.bat
@@ -1,9 +1,9 @@
 @echo off
-set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.6
+set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9
 set MSVC_PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.41.34120\bin\Hostx64\x64
 set OUTPUT_DIR=C:\Users\tonil\Repositories\Hoominer\releases\hoominer\cubins
 mkdir "%OUTPUT_DIR%"
-for %%a in (sm_50 sm_52 sm_60 sm_61 sm_70 sm_75 sm_80 sm_86 sm_89 sm_90 sm_100 sm_120) do (
-    nvcc -cubin -arch=%%a -fmad=false -prec-div=true -expt-relaxed-constexpr ^
-         -ccbin "%MSVC_PATH%" -I"%CUDA_PATH%\include" hoohash.cu -o "%OUTPUT_DIR%\hoohash_%%a.cubin"
+for %%a in (50 52 60 61 70 75 80 86 89 90 100 120) do (
+    nvcc -cubin -arch=compute_%%a -code=sm_%%a -fmad=false ^
+         -ccbin "%MSVC_PATH%" -I"%CUDA_PATH%\include" hoohash.cu -o "%OUTPUT_DIR%\hoohash_sm_%%a.cubin"
 )
